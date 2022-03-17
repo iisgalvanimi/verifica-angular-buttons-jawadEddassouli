1bc5de9b62254290de9767a2a576737cfb853c0a
jawadEddassouli <eddassouli.jawad@iisgalvanimi.edu.it>
2022-03-17 09:03:06 +0000

    375 - Verifica05 - Angular Data Binding (Buttons)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+
+  list: string[] = []
+  testo1:string="BUTTON01";
+  testo2:string="BUTTON02";
+  testo3:string="BUTTON03";
+  testo4:string="BUTTON04";
+
+  ultimo:string = ""
+
+  onClick1(){
+    this.list.push(this.testo1)
+    this.ultimo = this.testo1
+  }
+
+  onClick2(){
+    this.list.push(this.testo2)
+    this.ultimo = this.testo2
+  }
+
+  onClick3(){
+    this.list.push(this.testo3)
+    this.ultimo = this.testo3
+  }
+
+  onClick4(){
+    this.list.push(this.testo4)
+    this.ultimo = this.testo4
+  }
+
+
+
+
 }
